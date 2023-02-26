6d170589b4ccf1a38b8a7d98f0aff24838f01c80
leo <leo.a1025@gmail.com>
2023-02-26 23:02:38 +0800

    designing allmenu components

--- src/pages/AllMenu.tsx
+++ src/pages/AllMenu.tsx
@@ -1,282 +1,102 @@
-import { Text, Image, Stack, Button, Heading } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import {
+  Text,
+  Image,
+  Stack,
+  Button,
+  Heading,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
+import { Card, CardBody, CardFooter } from "@chakra-ui/react";
 import { store1 } from "../constants";
 
 export const AllMenu = () => {
+  const menuList = [
+    {
+      img: store1,
+      title: "Mango smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "480円",
+    },
+    {
+      img: store1,
+      title: "apple smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "500円",
+    },
+    {
+      img: store1,
+      title: "peach smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "600円",
+    },
+    {
+      img: store1,
+      title: "strawberry smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "380円",
+    },
+    {
+      img: store1,
+      title: "strawberry smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "380円",
+    },
+    {
+      img: store1,
+      title: "strawberry smoothie",
+      description:
+        "Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.",
+      price: "380円",
+    },
+  ];
+
   return (
     <>
-      <Text textAlign={"center"} p={"16px"} fontSize={"32px"}>
-        メニュー一覧
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
+      <Box bgColor={"orange.100"}>
+        <Text textAlign={"center"} p={"16px"} fontSize={"32px"}>
+          メニュー一覧
+        </Text>
+        <Box>
+          <Flex flexWrap={"wrap"} justify={"center"}>
+            {menuList.map((item) => (
+              <Text fontSize={"28px"} p={"16px"} textAlign={"center"}>
+                <Card
+                  direction={{ base: "column", sm: "row" }}
+                  overflow="hidden"
+                  variant="outline"
+                  w={"600px"}
+                  h={"280px"}
+                >
+                  <Image
+                    objectFit="cover"
+                    maxW={{ base: "100%", sm: "300px" }}
+                    src={item.img}
+                  />
+                  <Stack>
+                    <CardBody p={"2px"} pt={"16px"}>
+                      <Heading size="md">{item.title}</Heading>
+                      <Text fontSize={"24px"} pt={"8px"}>
+                        {item.description}
+                      </Text>
+                    </CardBody>
+                    <CardFooter>
+                      <Button variant="solid" colorScheme="blue">
+                        {item.price}
+                      </Button>
+                    </CardFooter>
+                  </Stack>
+                </Card>
               </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                480円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
-      <Text fontSize={"28px"} p={"20px"} textAlign={"center"}>
-        <Card
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: "100%", sm: "300px" }}
-            src={store1}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">The perfect latte</Heading>
-
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                500円
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      </Text>
+            ))}
+          </Flex>
+        </Box>
+      </Box>
     </>
   );
 };
